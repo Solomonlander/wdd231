47885ee5285816b05f73fd807bac7a6c583eee40
Landeremmanson <solomonemmanson491@gmail.com>
2026-01-23 17:58:41 +0100

    ........

--- chamber/js/directory.js
+++ chamber/js/directory.js
@@ -1,6 +1,12 @@
 const directory = document.getElementById("directory");
 const gridBtn = document.getElementById("gridBtn");
 const listBtn = document.getElementById("listBtn");
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
+
+hamburger.addEventListener("click", () => {
+    navLinks.classList.toggle("open");
+});
 
 async function loadMembers() {
     const response = await fetch("data/members.json");
@@ -13,16 +19,18 @@ function displayMembers(members) {
 
     members.forEach(member => {
         const card = document.createElement("section");
-        card.className = "member-card";
+        card.classList.add("member-card");
 
         card.innerHTML = `
       <img src="images/${member.image}" alt="${member.name}">
-      <h2>${member.name}</h2>
-      <p>${member.address}</p>
-      <p>${member.phone}</p>
-      <a href="${member.website}" target="_blank">Visit Website</a>
-      <p>Membership Level: ${member.level}</p>
-      <p>${member.description}</p>
+      <div>
+        <h2>${member.name}</h2>
+        <p>${member.address}</p>
+        <p>${member.phone}</p>
+        <a href="${member.website}" target="_blank">Visit Website</a>
+        <p>Membership Level: ${member.level}</p>
+        <p>${member.description}</p>
+      </div>
     `;
         directory.appendChild(card);
     });
